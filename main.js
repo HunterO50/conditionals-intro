b39ddccdfe4e1b2b6b88630e86969f9c1dc743fb
Hunter Owens <85654096+HunterO50@users.noreply.github.com>
2023-08-30 13:40:23 -0400

    Completed conditionals intro

---
+++ main.js
@@ -0,0 +1,67 @@
+// 1. Check Your Math
+/*
+if (1 + 1 > 5) {
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if (4 * 5 <= 20) { 
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if (6-2 >= 0) { 
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+//2. Dog People vs. Cat People 
+const likeDogs = true 
+if (likeDogs) {
+    console.log("You're a dog person!")
+    
+} else {
+    console.log("You're a cat person!")    
+} 
+*/
+
+// 3. Greetings 
+/*
+const person = {
+    name: "Hunter",
+    language: "English"
+}
+if (person.language === "English") {
+    console.log("Hello, Hunter!")
+    
+}  else if (person.language === "Spanish") {
+    console.log("Hola, Hunter!")
+}
+else if (person.language === "French")
+{
+console.log("Bonjour, Hunter!")
+}
+    */
+
+// 4. Student Grades
+let grade = 75
+
+if (grade >= 0 && grade <= 69) { 
+console.log("You got a F")
+}
+ else if (grade >= 70 && grade <= 76) {
+    console.log("You got a D")}
+
+    else if (grade >= 77 && grade <= 84) {
+      console.log("You got a C")}
+
+     else if (grade >= 84 && grade <=  92) {
+      console.log("You got a B")} 
+
+      else if (grade >= 93 && grade <= 100) {
+        console.log("You got a A")}
+
+
